351a5fb7c53b9a518bc96d4e252f7cffe1ac2192
Oksana <demskaya01@gmail.com>
2022-11-11 17:33:40 +0300

    gRPC Owners

---
+++ Auto.OwnerGrpcClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using Auto.OwnerGrpcService;
+using Grpc.Net.Client;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7258");
+var grpcClient = new OwnerService.OwnerServiceClient(channel);
+Console.WriteLine("Ready! Input registration: ");
+while (true)
+{
+    var registration = Console.ReadLine();
+    var request = new OwnerRequest()
+    {
+        Registration = registration
+    };
+    try
+    {
+        var reply = grpcClient.GetOwner(request);
+        Console.WriteLine($"Owner:  {reply.FirstName}  {reply.LastName}  {reply.PhoneNumber}");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Error!!! {e.Message}");
+    }
+   
+    Console.WriteLine("Input registration: ");
+}
---
+++ Auto.OwnerGrpcService/Services/OwnerService.cs
@@ -0,0 +1,24 @@
+﻿using Auto.Data;
+using Grpc.Core;
+
+namespace Auto.OwnerGrpcService.Services;
+
+public class OwnerService : OwnerGrpcService.OwnerService.OwnerServiceBase
+{
+    private readonly IAutoDatabase _db;
+
+    public OwnerService(IAutoDatabase db)
+    {
+        _db = db;
+    }
+
+    public override Task<OwnerReply> GetOwner(OwnerRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new OwnerReply
+        {
+            FirstName = "FirstName: " + _db.FindOwner(request.Registration).FirstName,
+            LastName = "LastName: " + _db.FindOwner(request.Registration).LastName,
+            PhoneNumber = "Phone number: " + _db.FindOwner(request.Registration).PhoneNumber
+        });
+    }
+}
