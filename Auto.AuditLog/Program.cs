057436ee4393c5a3e3e7aaa3ffe0367939df388b
Oksana <demskaya01@gmail.com>
2022-11-11 05:25:18 +0300

    RabbitMQ messages

---
+++ Auto.AuditLog/Program.cs
@@ -0,0 +1,35 @@
+﻿using Auto.Messages;
+using EasyNetQ;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    private static readonly IConfigurationRoot config = ReadConfiguration();
+    private const string SUBSCRIBER_ID = "Auto.AuditLog";
+
+    private static IConfigurationRoot ReadConfiguration()
+    {
+        var basePath = Directory.GetParent(AppContext.BaseDirectory).FullName;
+        return new ConfigurationBuilder()
+            .SetBasePath(basePath)
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+    }
+
+    static async Task Main(string[] args)
+    {
+        using var bus = RabbitHutch.CreateBus(config.GetConnectionString("AutoRabbitMQ")!);
+        Console.WriteLine("Connected! Listening for NewOwnerMessage messages.");
+        await bus.PubSub.SubscribeAsync<NewOwnerMessage>(SUBSCRIBER_ID, HandleNewOwnerMessage);
+        Console.ReadKey(true);
+    }
+
+    private static void HandleNewOwnerMessage(NewOwnerMessage message)
+    {
+        var csv =
+            $"{message.FirstName}, {message.LastName}, {message.PhoneNumber}, {message.TimeNow:O}";
+        Console.WriteLine(csv);
+    }
+}
+
---
+++ Auto.Messages/NewOwnerMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Auto.Messages;
+
+public class NewOwnerMessage
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime TimeNow { get; set; }
+}
